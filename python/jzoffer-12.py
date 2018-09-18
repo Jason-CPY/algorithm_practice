0861f0e689ccd55503ef0ca0720c9809125b4617
Jason-CPY <chpy_work@163.com>
2018-09-18 14:40:39 +0800

    浮点数乘方

---
+++ python/jzoffer-12.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Power(self, base, exponent):
+        # write code here
+        if exponent == 0:
+            return 1
+        if exponent == 1:
+            return base
+        if exponent == -1:
+            return 1/base
+        return base**exponent
